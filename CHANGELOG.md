ad1c5a96292af148cb655c35750a5172e5022e8a
Nadim Kobeissi <nadim@nadim.computer>
2015-01-15 00:20:34 +0100

    Bump to 1.0.2 (3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+##Peerio 1.0.2 (3)
+**January 15 2015**
+
+- Remove Import Contacts functionality pending bug fixes.
+
 ##Peerio 1.0.1 (2)
 **January 14 2015**
 
--- src/chrome/js/peerio/config.js
+++ src/chrome/js/peerio/config.js
@@ -10,8 +10,8 @@ Peerio.config = {};
 	'use strict';
 
 	Peerio.config = {
-		version: '1.0.1',
-		buildID: 2,
+		version: '1.0.2',
+		buildID: 3,
 		updateJSON: 'https://peerio.com/update/info.json',
 		updateWin: 'https://peerio.com/download/peerio-win.exe',
 		updateMac: 'https://peerio.com/download/peerio-mac.zip',
