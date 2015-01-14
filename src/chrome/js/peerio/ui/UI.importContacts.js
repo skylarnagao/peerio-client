77d8379581af2e58f9b52453d39df9f216da2b2e
Nadim Kobeissi <nadim@nadim.computer>
2015-01-14 19:57:48 +0100

    Fixes related to #10

--- src/chrome/js/peerio/ui/UI.importContacts.js
+++ src/chrome/js/peerio/ui/UI.importContacts.js
@@ -12,35 +12,47 @@ Peerio.UI.controller('importContacts', function($scope) {
 			complete: function(results) {
 				var invite = []
 				results.data.forEach(function(contact) {
-					(Object.keys(contact)).forEach(function(key) {
-						if (key.match(/(mail)|(phone)/i)) {
-							var parsed = Peerio.util.parseAddress(contact[key])
+					var keys = Object.keys(contact)
+					for (var i in keys) {
+						if (keys[i].match(/mail/i)) {
+							var parsed = Peerio.util.parseAddress(contact[keys[i]])
 							if (parsed) {
 								invite.push({
 									address: parsed
 								})
+								continue
 							}
 						}
-					})
+					}
 				})
-				Peerio.network.addContact(invite, function(data) {
-					$('form.importContactsUploadForm input[type=reset]').click()
-					if (({}).hasOwnProperty.call(data, 'error')) {
-						swal({
-							title: document.l10n.getEntitySync('importContactsError').value,
-							text: document.l10n.getEntitySync('importContactsErrorText').value,
-							type: 'error',
-							confirmButtonText: document.l10n.getEntitySync('OK').value
-						})
-					}
-					else {
-						swal({
-							title: document.l10n.getEntitySync('importContactsSuccess').value,
-							text: document.l10n.getEntitySync('importContactsSuccessText').value,
-							type: 'success',
-							confirmButtonText: document.l10n.getEntitySync('OK').value
-						})
-					}
+				swal({
+					title: document.l10n.getEntitySync('importContactsConfirm').value,
+					text: document.l10n.getEntitySync('importContactsConfirmText').value,
+					type: 'warning',
+					showCancelButton: true,
+					cancelButtonText: document.l10n.getEntitySync('cancel').value,
+					confirmButtonColor: '#e07a66',
+					confirmButtonText: document.l10n.getEntitySync('ok').value
+				}, function() {
+					Peerio.network.addContact(invite, function(data) {
+						$('form.importContactsUploadForm input[type=reset]').click()
+						if (({}).hasOwnProperty.call(data, 'error')) {
+							swal({
+								title: document.l10n.getEntitySync('importContactsError').value,
+								text: document.l10n.getEntitySync('importContactsErrorText').value,
+								type: 'error',
+								confirmButtonText: document.l10n.getEntitySync('OK').value
+							})
+						}
+						else {
+							swal({
+								title: document.l10n.getEntitySync('importContactsSuccess').value,
+								text: document.l10n.getEntitySync('importContactsSuccessText').value,
+								type: 'success',
+								confirmButtonText: document.l10n.getEntitySync('OK').value
+							})
+						}
+					})
 				})
 			}
 		})
