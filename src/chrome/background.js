fd7aeea354033c7dc2e751cfeb796eef2aa0f32f
Nadim Kobeissi <nadim@nadim.computer>
2015-01-22 23:04:53 +0100

    Window size tweaks

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,11 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-	var startingWidth  = window.screen.availWidth  - 500
-	var startingHeight = window.screen.availHeight - 200
+	var startingWidth  = window.screen.availWidth  - 200
+	var startingHeight = window.screen.availHeight - 100
 	chrome.app.window.create('index.html', {
 		minWidth: 1100,
 		minHeight: 670,
-		maxWidth: 1920,
-		maxHeight: 1080,
+		maxWidth: 3840,
+		maxHeight: 2160,
 		width: startingWidth,
 		height: startingHeight,
 		resizable: true
