5abaf1706a9d2594915d08934350b97a6c0cd7a0
Florencia Herra-Vega <floh@flohdot.org>
2015-06-16 14:30:26 -0400

    ship msi instead of exe

--- application/js/peerio/config.js
+++ application/js/peerio/config.js
@@ -13,7 +13,7 @@ Peerio.config = {};
 		version: '1.1.0',
 		buildID: 10,
 		updateJSON: 'https://peerio.com/update/info.json',
-		updateWin: 'https://peerio.com/download/peerio-win.exe',
+		updateWin: 'https://peerio.com/download/peerio-win.msi',
 		updateMac: 'https://peerio.com/download/peerio-mac.zip',
 		minPINEntropy: 24,
 		fileUploadSizeLimit: 419430400
