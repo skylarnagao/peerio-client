9bd24376a0183d3159dcc80fa8ac47aa851d34af
Florencia Herra-Vega <floh@flohdot.org>
2016-02-25 12:53:28 -0500

    bump to 1.3.1

--- application/js/peerio/config.js
+++ application/js/peerio/config.js
@@ -10,8 +10,8 @@ Peerio.config = {};
 	'use strict';
 
 	Peerio.config = {
-		version: '1.3.0',
-		buildID: 18,
+		version: '1.3.1',
+		buildID: 19,
 		updateJSON: 'https://peerio.com/update/info.json',
 		updateWin: 'https://peerio.com/download/peerio-win.msi',
 		updateMac: 'https://peerio.com/download/peerio-mac.zip',
