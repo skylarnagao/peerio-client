0ccfb1290a4d148e129736957352c1aa7ee8064a
Anri Asaturov <anri82@gmail.com>
2016-04-02 21:27:21 +0300

    4 char code

--- application/js/peerio/ui/UI.accountSettings.js
+++ application/js/peerio/ui/UI.accountSettings.js
@@ -98,6 +98,10 @@ Peerio.UI.controller('accountSettings', function ($scope) {
     }
     $scope.accountSettings.confirmAddress = function (event) {
         var thisAddress = $(event.target).prev('input').val()
+        if (event.target.value.length < 4) {
+            return false
+        }
+
         return actualConfirm(thisAddress, event.target.value);
     }
 
